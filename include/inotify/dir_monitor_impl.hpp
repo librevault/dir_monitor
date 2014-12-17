152781f1ce2d6e0e8bf2728f7b70f755d92f4779
Jiawen Zhang <jzhang@topologyinc.com>
2014-12-17 11:56:18 -0500

    Use IN_CLOSE_WRITE as the modified event in linux
    IN_MODIFY event will be fired multiple times (about 10 times
    when creating a 1MB file). This causes lots of unnecessary db update.
    We only want one modify event when creating a new file. IN_CLOSE_WRITE
    seems to do the right job. It means file opened for writing was closed.
    This is almost what we want, except for files that are opened for
    wrting for a long time and are never closed.

--- include/inotify/dir_monitor_impl.hpp
+++ include/inotify/dir_monitor_impl.hpp
@@ -43,7 +43,7 @@ public:
 
     void add_directory(const std::string &dirname)
     {
-        int wd = inotify_add_watch(fd_, dirname.c_str(), IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO);
+        int wd =  inotify_add_watch(fd_, dirname.c_str(), IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO | IN_CLOSE_WRITE);
         if (wd == -1)
         {
             boost::system::system_error e(boost::system::error_code(errno, boost::system::get_system_category()), "boost::asio::dir_monitor_impl::add_directory: inotify_add_watch failed");
@@ -162,9 +162,9 @@ private:
                 {
                 case IN_CREATE: type = dir_monitor_event::added; break;
                 case IN_DELETE: type = dir_monitor_event::removed; break;
-                case IN_MODIFY: type = dir_monitor_event::modified; break;
                 case IN_MOVED_FROM: type = dir_monitor_event::renamed_old_name; break;
                 case IN_MOVED_TO: type = dir_monitor_event::renamed_new_name; break;
+                case IN_CLOSE_WRITE: type = dir_monitor_event::modified; break;
                 case IN_CREATE | IN_ISDIR:
                     {
                         type = dir_monitor_event::added;
@@ -210,4 +210,3 @@ private:
 
 }
 }
-
